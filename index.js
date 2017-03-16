d4db5ecfb5a50bfba04986c4f23d88403fd347ce
jonschlinkert <jon.schlinkert@sellside.com>
2017-03-16 11:00:28 -0400

    minor edits

--- index.js
+++ index.js
@@ -3,10 +3,9 @@
 var sync = require('sync-pkg');
 
 // sync-pkg will eventually sync with more than bower.json,
-// at which point we will probably move the bower code
-// to this library, then require it back to sync-pkg.
-// For now we're going to pass it through and use this as
-// a facade.
+// at which point we will probably move the bower code to this
+// library, then require it back to sync-pkg. For now we're going
+// to pass it through and use this as a facade.
 module.exports = function() {
   return sync.bower.apply(sync, arguments);
 };
